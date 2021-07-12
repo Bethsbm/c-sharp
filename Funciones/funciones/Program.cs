f3efa0d29097960450cccc9192d112355464daab
Maritza Sosa <maritza_sosa@live.com>
2021-07-12 00:32:59 -0600

    Subir funciones

---
+++ Funciones/funciones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace funciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno (1, "Alexha", "Betsabe");
+            Alumno b = new Alumno (2, "Irving", "Antonio");
+            
+            a.inactivarAlumno();
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+
+            a.activarAlumno();
+
+            Console.WriteLine(a.nombreCompleto());
+            Console.WriteLine(b.nombreCompleto());
+        }
+    }
+}
