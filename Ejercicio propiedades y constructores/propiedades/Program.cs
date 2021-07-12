1ddc61f5c5fe774371bd939d4e4c3be0287552c6
Maritza Sosa <maritza_sosa@live.com>
2021-07-11 23:52:26 -0600

    Subir E-Propiedades y Constructores

---
+++ Ejercicio propiedades y constructores/propiedades/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace propiedades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno a = new Alumno();
+            a. PrimerNombre = "Maritza";
+            a. SegundoNombre = "Betsabe";
+
+            Alumno b = new Alumno();
+            b. Id = 2;
+            b. PrimerNombre = "Alexha";
+            b. SegundoNombre = "Betsabe";
+
+            Alumno c = new Alumno(3);
+            b. PrimerNombre = "Kristhian";
+            b. SegundoNombre = "Josue";
+
+            Alumno d = new Alumno("Irving","Antonio");
+
+            Console.WriteLine(a.Id);
+            Console.WriteLine(b.Id);
+            Console.WriteLine(c.Id);
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
+            
+        }
+    }
+}
